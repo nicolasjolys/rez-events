89875f0900636ae8b85852c30a31e8dc35200014
Melia Mazouzi <melia.mazouzi@gmail.com>
2021-11-30 16:44:37 +0100

    Finished event associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
-  # DISTRICTS = %w[]
+  DISTRICTS = ['La Blordière', 'Château La Houssais', 'Pont-Rousseau', 'Ragon', 'Hôtel de ville', 'Trentemoult-les Isles']
+  SAFETY_LEVELS = ['Niveau I', 'Niveau II', 'Niveau III']
   STATUSES = %w[pending accepted declined]
 
   belongs_to :user
@@ -7,14 +8,20 @@ class Event < ApplicationRecord
   has_many :participants, through: :event_participations, source: :user
 
   validates :name,
+            :public_description,
             :district,
             :address,
             :category,
             :start_at,
             :end_at,
+            :contact_first_name,
+            :contact_last_name,
+            :contact_email,
+            :contact_phone_number,
             :required_safety_level,
             :pricing_description,
             :status,
             presence: true
   validates :status, inclusion: { in: STATUSES }
+  validates :district, inclusion: { in: DISTRICTS }
 end
