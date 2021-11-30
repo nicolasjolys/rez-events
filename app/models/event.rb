2df711bfe9ec8657871607d5eb57538bd135d361
Melia Mazouzi <melia.mazouzi@gmail.com>
2021-11-30 15:48:47 +0100

    Adding validations on event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,15 @@
 class Event < ApplicationRecord
+  belongs_to :user
+  has_many :city_hall_participants, through: :user
+
+  validates :name,
+            :district,
+            :address,
+            :category,
+            :start_at,
+            :end_at,
+            :required_safety_level,
+            :pricing_description,
+            :status,
+            presence: true
 end
