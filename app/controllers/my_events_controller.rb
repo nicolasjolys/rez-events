6d9049a936ebee24329d2ec9ff49a5d4dbfc66fc
Melia Mazouzi <melia.mazouzi@gmail.com>
2021-12-01 07:24:44 +0100

    Add US index my_events and new event

--- app/controllers/my_events_controller.rb
+++ app/controllers/my_events_controller.rb
@@ -8,4 +8,17 @@ class MyEventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    @event = Event.new(event_params)
+    @user = current_user
+    @event.user = @user
+    @event.save
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit()
+    # Voir si possible de simplifier la liste des params
+  end
 end
