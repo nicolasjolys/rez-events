e96ae014e4fe1912c82ce66cc1e438039ea260ba
Melia Mazouzi <melia.mazouzi@gmail.com>
2021-12-01 16:10:51 +0100

    Route added for destroy, need to refacto

--- app/controllers/agent/event_participations_controller.rb
+++ app/controllers/agent/event_participations_controller.rb
@@ -5,5 +5,14 @@ class Agent::EventParticipationsController < ApplicationController
     @event_participation.event = Event.find(params[:event_id])
 
     @event_participation.save
+
+    redirect_to agent_events_path
+  end
+
+  def destroy
+    @event_participation = EventParticipation.find(params[:id])
+    @event_participation.destroy
+
+    redirect_to agent_events_path
   end
 end
