e34d724ca836c736cfece7e3c4469d88036b13c6
nicolas jolys <jolys.nicolas@gmail.com>
2021-12-03 13:11:58 +0100

    redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  # after_sign_in_path_for agent_admin_events_path
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || agent_admin_events_path
+  end
 end
