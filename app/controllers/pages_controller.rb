4ddfb1f2ec87363a90f9cb021bb6cfd5159ade70
Melia Mazouzi <melia.mazouzi@gmail.com>
2021-12-05 00:29:37 +0100

    No-repeat on background-img and basic marker deleted

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,18 @@ class PagesController < ApplicationController
 
   def home
     if params[:query].present?
-      sql_query = "name ILIKE :query OR public_description ILIKE :query"
+      sql_query = "name ILIKE :query OR public_description ILIKE :query OR category ILIKE :query"
       @events = Event.where(sql_query, query: "%#{params[:query]}%")
     else
       @events = Event.all
     end
-
 
     @markers = @events.geocoded.map do |event|
       {
         lat: event.latitude,
         lng: event.longitude,
         info_window: render_to_string(partial: "info_window", locals: { event: event }),
-        image_url: helpers.asset_url("#{event.category}.png")
+        image_url: helpers.asset_url("#{event.category}.png"),
       }
     end
   end
