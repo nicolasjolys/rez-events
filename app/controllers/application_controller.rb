cbf731efa4b1e3a39444a3ef3c949827d39f5884
nicolas jolys <jolys.nicolas@gmail.com>
2021-12-04 23:51:03 +0100

    Redirect after login + nav back + home picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || agent_admin_events_path
+    if current_user.city_hall_admin
+      stored_location_for(resource) || agent_admin_events_path
+    else
+      stored_location_for(resource) || agent_events_path
+    end
   end
 end
