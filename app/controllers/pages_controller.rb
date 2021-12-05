c72c564f16298f2f403d68e864ca2cc5012030a2
Melia Mazouzi <melia.mazouzi@gmail.com>
2021-12-05 14:32:50 +0100

    Working on SVG picto

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,13 +8,12 @@ class PagesController < ApplicationController
     else
       @events = Event.all
     end
-
     @markers = @events.geocoded.map do |event|
       {
         lat: event.latitude,
         lng: event.longitude,
         info_window: render_to_string(partial: "info_window", locals: { event: event }),
-        image_url: helpers.asset_url("#{event.category}.png"),
+        # image_url: helpers.asset_url("#{event.category}.svg")
       }
     end
   end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -11,8 +11,10 @@ const addMarkersToMap = (map, markers) => {
     element.style.backgroundImage = `url('${marker.image_url}')`;
     element.style.backgroundRepeat = 'no-repeat';
     element.style.backgroundSize = 'contain';
-    element.style.width = '60px';
-    element.style.height = '60px';
+    element.style.backgroundColor = 'white';
+    element.style.borderRadius = '50%';
+    element.style.width = '20px';
+    element.style.height = '20px';
 
     // Pass the element as an argument to the new marker
     new mapboxgl.Marker()
@@ -25,7 +27,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
 
 
