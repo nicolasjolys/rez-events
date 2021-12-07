1a5dedc777cca42978e7452fe8cb7070ef855042
Melia Mazouzi <melia.mazouzi@gmail.com>
2021-12-07 15:46:08 +0100

    sweet alert on admin validations pages done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -25,11 +26,11 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 import { initMapbox } from '../plugins/init_mapbox';
-import { initSweetalert } from '../plugins/init_sweetalert';
-
+import { initSweetAlert } from "../plugins/init_sweetalert"
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initSweetAlert();
 });
--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,21 +1,52 @@
-import swal from 'sweetalert';
+import Swal from 'sweetalert2'
+import Rails from "@rails/ujs"
 
-const initSweetalert = (selector, options = {}, callback = () => { }) => {
-  const swalButton = document.querySelector(selector);
-  if (swalButton) { // protect other pages
-    swalButton.addEventListener('click', () => {
-      swal(options).then(callback); // <-- add the `.then(callback)`
-    });
-  }
-};
+export const initSweetAlert = () => {
+  const declineForms = document.querySelectorAll('form.decline-btn');
+  declineForms.forEach((form) => {
+    const button = form.querySelector('.btn-decline')
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      Swal.fire({
+        title: "Merci d'indiquer le motif du refus",
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Confirmer le refus',
+        cancelButtonText: 'Annuler'
 
-initSweetalert('#sweet-alert-demo', {
-  title: "A nice alert",
-  text: "This is a great alert, isn't it?",
-  icon: "success"
-}, (value) => {
-  console.log(value);
-});
-
-
-export { initSweetalert };
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const input = form.querySelector('.decline-btn input[name="declined_explanation"]')
+          console.log(form)
+          input.value = result.value
+          form.submit()
+          // Rails.ajax({
+          //   url: form.action,
+          //   type: form.firstElementChild.value
+          // })
+        }
+      })
+    })
+  })
+  const acceptEvent = document.querySelectorAll(".accept-btn")
+  acceptEvent.forEach((form) => {
+    const button = form.querySelector('.btn-accept')
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      Swal.fire({
+        icon: 'success',
+        title: 'Evénement validé!',
+        showConfirmButton: false,
+        timer: 1500
+    }).then((result) => {
+        console.log(result)
+        if (result.isDismissed) {
+          form.submit()
+        }
+      })
+    })
+  })
+}
