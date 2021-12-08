7a1ad961ab0a7c56f7bc23ea658d9a7a5701d8e1
nicolas jolys <jolys.nicolas@gmail.com>
2021-12-08 11:01:51 +0100

    Set seeds pictures + Inser pictures in cards

--- app/javascript/plugins/select_photo.js
+++ app/javascript/plugins/select_photo.js
@@ -1,14 +1,16 @@
 const selectPhoto = () => {
   const target = document.getElementById("file-ip-1");
 
-  target.addEventListener("change", (event) => {
-    if (event.target.files.length > 0) {
-      let src = URL.createObjectURL(event.target.files[0]);
-      let preview = document.getElementById("file-ip-1-preview");
-      preview.src = src;
-      preview.style.display = "block";
-    }
-  })
+  if (target) {
+    target.addEventListener("change", (event) => {
+      if (event.target.files.length > 0) {
+        let src = URL.createObjectURL(event.target.files[0]);
+        let preview = document.getElementById("file-ip-1-preview");
+        preview.src = src;
+        preview.style.display = "block";
+      }
+    })
+  }
 };
 
 export { selectPhoto };
